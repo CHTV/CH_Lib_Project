18453cb60c550597de5e9028176a97a8356a07a7
Yuxianglong <xianglong.yu@changhong.com>
2017-07-16 22:23:53 +0800

    add variables define class

---
+++ ch_lib/src/main/java/com/changhong/watson/common/VarDef.java
@@ -0,0 +1,10 @@
+package com.changhong.watson.common;
+
+/**
+ * Define some variables
+ * Created by Watson on 2017/7/16.
+ */
+
+public class VarDef {
+    public final static String PATH_STAT = "/proc/stat";
+}
